5c1b878529dab09afbb1786ef5d2a7d6f17341a2
RudolfJB <you.invention@gmail.com>
2023-03-09 09:47:22 +0200

    lesson_1_1

--- Lesson_1/main.py
+++ Lesson_1/main.py
@@ -5,14 +5,21 @@ class first_oop():
     def __init__(self, **kw):
         super(first_oop, self).__init__(**kw)
 
+    def call_back(self, data):
+        # print("call back", str(data))
+        cb_list = data.split(" ")
+        for i in cb_list:
+            print("cb: ", str(i))
+
     def main(self):
         print("first name")
         while True:
             cmd = input("command>>")
             if "Q" == cmd.upper():
                 return
-            else:
-                print(cmd)
+
+            if "cb" in cmd:
+                self.call_back(cmd)
 
 
 if __name__ == "__main__":
